a303cc0edc2d51274aa91f5296b317a4d592e204
Tom Lehman <tom.s.lehman@gmail.com>
2025-10-16 18:22:42 -0400

    Remove old files

--- app/controllers/application_controller.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationController < ActionController::API
-  def self.cache_actions_on_block(...)
-  end
-end
--- config/initializers/array_extensions.rb
+++
@@ -1,5 +0,0 @@
-class Array
-  def to_cache_key(namespace = nil)
-    ActiveSupport::Cache.expand_cache_key(self, namespace)
-  end
-end
